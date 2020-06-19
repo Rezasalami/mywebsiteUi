1a131e11d024e729cfd545b96632b13ad2b90d9c
Rezasalami <salami1365@gmail.com>
2020-06-19 21:13:52 +0430

    add font awesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import logo from './img/logo.png';
 import Footer from './Component/Footer'
 
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 class Footer extends Component {
     render() {
         let today = new Date()
         return (
             <footer className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full  ">
+                    <FontAwesomeIcon icon={faCoffee} />
                  	&copy; Copyright  2020 <a href="Https://Rezasalami.ir">Rezasalami.ir</a>
             </footer>
         );
