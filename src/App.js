277d7689c35a303b5fb6123bd861991c05d71d1e
Rezasalami <salami1365@gmail.com>
2020-06-21 18:37:15 +0430

    add slide

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import logo from './img/logo.png';
 import Footer from './Component/Footer'
 import Navbar from './Component/Navbar'
+import Slide from './Component/Slide'
 
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Slide></Slide>
       <Footer></Footer>
     </div>
 
